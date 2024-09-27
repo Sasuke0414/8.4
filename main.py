3280e1ed83176c74bcbaf0387140a19238c1d43d
Abdulloh <163301172+Sasuke0414@users.noreply.github.com>
2024-09-27 22:36:18 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# print("5 ta Son kiriting: ")
+
+# son1 = input("son1: ")
+# son2 = input("son2: ")
+# son3 = input("son3: ")
+# son4 = input("son4: ")
+# son5 = input("son5: ")
+
+# sonlar = []
+
+# for i in range(1, 11):
+#     son = int(input(f"Son{i}: "))
+#     sonlar.append(son)
+
+# print(sonlar)
+#  ------------------------------------------------------------------------
+
+# royhat = [2, 4, 7, 8, 9999999999999999]
+
+# javob = 0
+
+# for i in royhat: # 
+#     javob += i 
+
+# print(javob)
+
+
+#---------------------------------------------------------
+
+# sonlar = [1, 2, 3, 4]
+
+# uzunlik = len(sonlar)
+
+# index = 0
+
+# while index != uzunlik - 1
+#     if sonlar[index] % 2 == 0
+#     break
+
+# print(sonlar[index])
